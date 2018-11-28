92f096da2f23f569cd4afafc75a1c735c2a48567
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-28 12:22:23 +0800

    Add index and uniqueness validation to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: {case_insensitive: false}
   has_secure_password
 end
