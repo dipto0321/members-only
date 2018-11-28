af9e89b0be8b62272ff0f48b179da440ca97ec33
dipto0321 <diptokmk47@gmail.com>
2018-11-28 10:59:19 +0600

    Initial task for sessions
        * Create sessions controller with necessary files
        * Add create function
        * Create sign in template

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
   resources :users
 end
