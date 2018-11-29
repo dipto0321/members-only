2c2580ac806750a902fd2eb86474c49f1f556947
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-11-29 10:21:34 +0800

    Put cancel button on new and edit form for posts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,10 +5,13 @@ module ApplicationHelper
   end
 
   def cancel_path
-    params[:action] == 'edit' ? user_path : root_path
+    cancel_redirect_path = params[:controller] == "posts" ? user_path(current_user.id) : root_path 
+
+    params[:action] == 'edit' ? user_path(current_user.id): cancel_redirect_path
   end
 
   def display_delete
+    
     params[:action] == 'edit' ? nil : 'display-none'
   end
 end
