846b951d32eed73d187e2a16756dd7379096386d
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2018-12-07 17:18:21 +0800

    Create default scope for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
+
+  default_scope {order(created_at: :desc)}
 end
